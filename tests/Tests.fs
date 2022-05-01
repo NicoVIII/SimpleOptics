0d22b11921519b6fc0e39d2eeba2176e455a291a
NicoVIII <nico@nicoviii.net>
2022-05-01 10:15:38 +0000

    Add workaround for missing testProperty support in Fable.Mocha

--- tests/Tests.fs
+++ tests/Tests.fs
@@ -9,9 +9,23 @@ open Expecto
 open SimpleOptics
 open SimpleOptics.Presets
 
-// Stub for fable for testProperty
+// Workaround for Fable for testProperty
+[<AbstractClass>]
+type PropertyTest() =
 #if FABLE_COMPILER
-let testProperty (label: string) (fnc: 'a -> bool) = test $"[not functional] {label}" { () }
+    static member Create(label, fnc: int -> bool) =
+        test label {
+            for x in [ 0; 1; -1; 1000; -1000 ] do
+                Expect.isTrue (fnc x) $"Property test is not true with value {x}"
+        }
+
+    static member Create(label, fnc: string -> bool) =
+        test label {
+            for x in [ ""; "testtesttesttest"; "\"²§" ] do
+                Expect.isTrue (fnc x) $"Property test is not true with value {x}"
+        }
+#else
+    static member Create(label, fnc: 'a -> bool) = testProperty label fnc
 #endif
 
 let tests =
@@ -26,14 +40,16 @@ let tests =
                         Expect.isNone result "Get on an empty list should return None"
                     }
 
-                    testProperty "set >> get"
-                    <| fun (value: int) ->
-                        let readValue =
-                            [ 123 ]
-                            |> Optic.set (ListOptic.index 0) value
-                            |> Optic.get (ListOptic.index 0)
+                    PropertyTest.Create(
+                        "set >> get",
+                        fun (value: int) ->
+                            let readValue =
+                                [ 123 ]
+                                |> Optic.set (ListOptic.index 0) value
+                                |> Optic.get (ListOptic.index 0)
 
-                        readValue = Some value
+                            readValue = Some value
+                    )
                 ]
 
             testList
@@ -44,14 +60,16 @@ let tests =
                         Expect.isNone result "Get on an empty array should return None"
                     }
 
-                    testProperty "set >> get"
-                    <| fun (value: int) ->
-                        let readValue =
-                            [| 123 |]
-                            |> Optic.set (ArrayOptic.index 0) value
-                            |> Optic.get (ArrayOptic.index 0)
+                    PropertyTest.Create(
+                        "set >> get",
+                        fun (value: int) ->
+                            let readValue =
+                                [| 123 |]
+                                |> Optic.set (ArrayOptic.index 0) value
+                                |> Optic.get (ArrayOptic.index 0)
 
-                        readValue = Some value
+                            readValue = Some value
+                    )
                 ]
 
             testList
@@ -62,24 +80,27 @@ let tests =
                         Expect.isNone result "Get on an empty map should return None"
                     }
 
-                    testProperty "set >> get"
-                    <| fun (value: string) ->
-                        let readValue =
-                            new Map<int, string>([])
-                            |> Optic.set (MapOptic.find 0) value
-                            |> Optic.get (MapOptic.find 0)
+                    PropertyTest.Create(
+                        "set >> get",
+                        fun (value: string) ->
+                            let readValue =
+                                new Map<int, string>([])
+                                |> Optic.set (MapOptic.find 0) value
+                                |> Optic.get (MapOptic.find 0)
 
-                        readValue = Some value
+                            readValue = Some value
+                    )
                 ]
 
             testList
                 "Optic compose"
                 [
-                    test "basic compose" {
-                        let constant = 5
-                        let optic = Optic.compose (ListOptic.index 0) (ListOptic.index 0)
-                        let result = Optic.get optic [ [ constant ] ]
-                        Expect.equal result (Some constant) "Composing optics should work"
-                    }
+                    PropertyTest.Create(
+                        "basic composition",
+                        fun (value: int) ->
+                            let optic = Optic.compose (ListOptic.index 0) (ListOptic.index 0)
+                            let result = Optic.get optic [ [ value ] ]
+                            result = (Some value)
+                    )
                 ]
         ]
